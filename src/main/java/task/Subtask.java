3c97561e9803ce9bc5199a2b330b7b4793d0783f
KAmi-1262579049 <camisha21@yandex.ru>
2026-06-02 13:44:26 +0300

    Update Subtask.java

--- src/main/java/task/Subtask.java
+++ src/main/java/task/Subtask.java
@@ -1,29 +1,47 @@
 package task;
 
-// Класс Subtask, представляющий подзадачу в системе трекера задач
-public class Subtask extends Task {
-    private int epicId; // Поле для хранения идентификатора эпика
+import java.time.Duration;
+import java.time.LocalDateTime;
 
-    // Конструктор для создания новой подзадачи без указания id
+// Класс Subtask наследуется от класса Task
+public class Subtask extends Task {
+    private int epicId; // Идентификатор эпика, к которому относится подзадача
+
+    // Конструктор создания подзадачи без времени начала и продолжительности
     public Subtask(String name, String description, int epicId) {
         super(name, description);
         this.epicId = epicId;
         this.type = TaskType.SUBTASK;
     }
 
-    // Конструктор для создания подзадачи с известным id
+    // Конструктор создания подзадачи с продолжительностью и временем начала
+    public Subtask(String name, String description, int epicId, Duration duration, LocalDateTime startTime) {
+        super(name, description, duration, startTime);
+        this.epicId = epicId;
+        this.type = TaskType.SUBTASK;
+    }
+
+    // Конструктор для восстановления подзадачи с уже существующим id
     public Subtask(int id, String name, String description, TaskStatus status, int epicId) {
         super(id, name, description, status);
         this.epicId = epicId;
         this.type = TaskType.SUBTASK;
     }
 
-    // Геттер для поля epicId
+    // Конструктор для восстановления подзадачи со всеми параметрами
+    public Subtask(int id, String name, String description, TaskStatus status, int epicId,
+                   Duration duration, LocalDateTime startTime) {
+        super(id, name, description, status, duration, startTime);
+        this.epicId = epicId;
+        this.type = TaskType.SUBTASK;
+    }
+
+    // Геттер для получения id эпика
     public int getEpicId() {
         return epicId;
     }
 
-    // Сеттер для поля epicId
+    // Сеттер для изменения id эпика
     public void setEpicId(int epicId) {
         this.epicId = epicId;
     }
@@ -37,6 +55,8 @@ public class Subtask extends Task {
                 ", description='" + description + '\'' +
                 ", status=" + status +
                 ", type=" + type +
+                ", duration=" + duration +
+                ", startTime=" + startTime +
                 ", epicId=" + epicId +
                 '}';
     }
