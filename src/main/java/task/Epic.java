88032eeda9c39ff4fb82ba16b2df79b37b23ba7d
KAmi-1262579049 <camisha21@yandex.ru>
2026-06-02 13:38:16 +0300

    Update Epic.java

--- src/main/java/task/Epic.java
+++ src/main/java/task/Epic.java
@@ -1,13 +1,15 @@
 package task;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-// Класс Epic наследуется от Task и представляет сложную задачу, состоящую из подзадач
 public class Epic extends Task {
-    private List<Integer> subtaskIds;
+    private final List<Integer> subtaskIds; // Список идентификаторов подзадач, принадлежащих эпику
+    private LocalDateTime endTime; // Время окончания эпика (рассчитывается по подзадачам)
 
-    // Конструктор для создания нового эпика (без id)
+    // Конструктор для создания нового эпика без id
     public Epic(String name, String description) {
         super(name, description);
         this.subtaskIds = new ArrayList<>();
@@ -15,33 +17,58 @@ public class Epic extends Task {
         this.status = TaskStatus.NEW;
     }
 
-    // Конструктор для создания эпика с заданными параметрами
+    // Конструктор для создания эпика с известным id
     public Epic(int id, String name, String description) {
         super(id, name, description, TaskStatus.NEW);
         this.subtaskIds = new ArrayList<>();
         this.type = TaskType.EPIC;
     }
 
-    // Геттер для получения списка идентификаторов подзадач
+    // Метод получения списка id подзадач
     public List<Integer> getSubtaskIds() {
         return new ArrayList<>(subtaskIds);
     }
 
-    // Метод для добавления идентификатора подзадачи в эпик
+    // Метод добавления id подзадачи
     public void addSubtaskId(int subtaskId) {
         if (!subtaskIds.contains(subtaskId)) {
             subtaskIds.add(subtaskId);
         }
     }
 
-    // Метод для удаления идентификатора подзадачи из эпика
+    // Метод удаления id подзадачи
     public void removeSubtaskId(int subtaskId) {
         subtaskIds.remove(Integer.valueOf(subtaskId));
     }
 
-    // Метод для очистки списка подзадач
+    // Метод удаления всех подзадач эпика
     public void clearSubtaskIds() {
         subtaskIds.clear();
+    }
+
+    // Метод вычисления времени продолжительности эпика
+    public void setCalculatedTime(Duration duration, LocalDateTime startTime, LocalDateTime endTime) {
+        this.duration = duration == null ? Duration.ZERO : duration;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    // Метод установки продолжительности
+    @Override
+    public void setDuration(Duration duration) {
+        throw new UnsupportedOperationException("Продолжительность эпика рассчитывается по подзадачам");
+    }
+
+    // Метод установки времени начала
+    @Override
+    public void setStartTime(LocalDateTime startTime) {
+        throw new UnsupportedOperationException("Время начала эпика рассчитывается по подзадачам");
+    }
+
+    // Метод получения времени окончания эпика
+    @Override
+    public LocalDateTime getEndTime() {
+        return endTime;
     }
 
     // Переопределение метода toString()
@@ -53,6 +80,9 @@ public class Epic extends Task {
                 ", description='" + description + '\'' +
                 ", status=" + status +
                 ", type=" + type +
+                ", duration=" + duration +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
                 ", subtaskIds=" + subtaskIds +
                 '}';
     }
