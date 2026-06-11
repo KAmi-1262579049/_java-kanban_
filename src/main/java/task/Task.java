c31726d17db676626518e5bd07ea0a37bbd21aad
KAmi-1262579049 <camisha21@yandex.ru>
2026-06-11 13:10:06 +0300

    Update Task.java

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -125,8 +125,12 @@ public class Task {
     // Переопределение метода сравнения объектов
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Task task = (Task) o;
         return id == task.id;
     }
