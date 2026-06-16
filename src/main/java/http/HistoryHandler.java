2c32cfb38aec5054c313aac35d490bfa91e71e37
KAmi-1262579049 <camisha21@yandex.ru>
2026-06-16 12:22:59 +0300

    Create HistoryHandler.java

---
+++ src/main/java/http/HistoryHandler.java
@@ -0,0 +1,30 @@
+package http;
+
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import manager.TaskManager;
+
+import java.io.IOException;
+
+// Класс HistoryHandler обрабатывает HTTP-запросы для получения истории просмотров задач
+public class HistoryHandler extends BaseHttpHandler implements HttpHandler {
+    private final TaskManager taskManager; // Хранит экземпляр менеджера задач
+    private final Gson gson; // Используется для преобразования объектов Java в JSON и обратно
+
+    // Конструктор класса
+    public HistoryHandler(TaskManager taskManager, Gson gson) {
+        this.taskManager = taskManager;
+        this.gson = gson;
+    }
+
+    // Метод обрабатывает входящие HTTP-запросы
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        if ("GET".equals(exchange.getRequestMethod()) && "/history".equals(exchange.getRequestURI().getPath())) {
+            sendText(exchange, gson.toJson(taskManager.getHistory()));
+            return;
+        }
+        sendNotFound(exchange);
+    }
+}
